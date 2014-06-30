986210d774f5e4f30e3706acb580049d6d1fd709
Boy <andong135@126.com>
2014-06-30 16:14:45 +0800

    改善上传交互

--- sites/all/themes/beauty/templates/page--beauty-world-cup-hair-upload.tpl.php
+++ sites/all/themes/beauty/templates/page--beauty-world-cup-hair-upload.tpl.php
@@ -2,14 +2,13 @@
   <div class="page-upload">
     <form id="image_upload" action="" method="post" enctype="multipart/form-data" >
       <input type="file" name="file" id="file" accept="image/*" capture="camera" >
-      <input id="submit" type="submit" name="submit" class="btn btn-upload" value="点击上传" >
+      <input id="submit" type="submit" name="submit" class="btn btn-upload" value="点击上传" style="display: none;">
     </form>
   </div>
 </div>
 <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
 <script type="text/javascript">
   $(document).ready(function() {
-    $('#submit').hide();
     $('#file').change(function() {
       if(this.value != '') {
         $('#submit').show();
