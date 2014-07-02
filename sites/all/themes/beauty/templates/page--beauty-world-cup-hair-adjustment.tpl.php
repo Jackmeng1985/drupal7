c7da9c888acbecefac14cb9ace858be112fedbb0
Boy <andong135@126.com>
2014-07-02 15:34:59 +0800

    save picture

--- sites/all/themes/beauty/templates/page--beauty-world-cup-hair-adjustment.tpl.php
+++ sites/all/themes/beauty/templates/page--beauty-world-cup-hair-adjustment.tpl.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $base_path = base_path();
 /*<?php print $base_path . $directory; ?>/*/
 ?>
@@ -6,13 +6,29 @@ $base_path = base_path();
   <a class="tab-item scaleup" href="#">放大</a>
   <a class="tab-item scaledown" href="#">缩小</a>
   <a class="tab-item" href="<?php print url('world_cup/hair'); ?>">更多发型</a>
-  <a class="tab-item share" href="#">分享</a>
+  <a id="share" class="tab-item share" href="#">分享</a>
 </nav>
 <div class="content">
   <div class="page-hair-adjustment">
        <canvas id="face" width="600" height = "600"> </canvas>
-       <canvas id ="hairChange" width="448" height="733" > </canvas>   
+       <canvas id ="hairChange" width="448" height="733" > </canvas>
        <canvas id="hairStyle" width="448" height="733"></canvas>
   </div>
 </div>
 <?php print($page['content']['system_main']['main']['#markup']);?>
+
+<script>
+(function($) {$(document).ready(
+  function() {
+    var share = document.getElementById('share');
+    share.addEventListener(endEvent, function() {
+      var canvas = document.getElementById('face');
+      if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+        var share_data = canvas.toDataURL();
+      }
+    }, false);
+  });
+})(jQuery);
+
+</script>
