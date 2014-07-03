4efc9e35b6063f52398144ae7706156d9ab825c2
Boy <andong135@126.com>
2014-07-03 11:53:54 +0800

    优化分享流程

--- sites/all/themes/beauty/templates/page--beauty-world-cup-hair-adjustment.tpl.php
+++ sites/all/themes/beauty/templates/page--beauty-world-cup-hair-adjustment.tpl.php
@@ -17,18 +17,10 @@ $base_path = base_path();
 <?php print($page['content']['system_main']['main']['#markup']);?>
 <script>
   WeixinApi.ready(function(Api) {
-      var canvas = document.getElementById('face');
-      var share_data = canvas.toDataURL();
-      $.ajaxSetup({
-          async : false
-      });
-      $.post('/world_cup/hair/share', {'image': share_data}, function(data) {
-        window.share_url = data.url;
-      }, 'json');
     // 微信分享的数据
     var wxData = {
         "appId": "wx3abd6037cb0edf82", // 服务号可以填写appId
-        "imgUrl" : window.share_url,
+        "imgUrl" : '',
         "link" : 'http://pfx.choosebeauty.com.cn/world_cup/hair',
         "desc" : '找到一个好玩的东西，可以换新发型玩',
         "title" : "发型由我"
@@ -38,7 +30,15 @@ $base_path = base_path();
     var wxCallbacks = {
         // 分享操作开始之前
         ready : function() {
-        },
+          var canvas = document.getElementById('face');
+          var share_data = canvas.toDataURL();
+          $.ajaxSetup({
+              async : false
+          });
+          $.post('/world_cup/hair/share', {'image': share_data}, function(data) {
+            window.share_url = data.url;
+          }, 'json');
+          wxData.imgUrl = window.share_url;
         // 分享被用户自动取消
         cancel : function(resp) {
             // 你可以在你的页面上给用户一个小Tip，为什么要取消呢？
