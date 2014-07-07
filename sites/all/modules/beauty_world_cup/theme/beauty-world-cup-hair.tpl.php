9c96aae3677b865a9756c24655421e8a788e89ec
jack meng <mengweifu1985@gmail.com>
2014-07-07 18:16:04 +0800

    #6 发型库， 分类区域增加更多|收起链接

--- sites/all/modules/beauty_world_cup/theme/beauty-world-cup-hair.tpl.php
+++ sites/all/modules/beauty_world_cup/theme/beauty-world-cup-hair.tpl.php
@@ -1,5 +1,6 @@
 <?php
-
+$i = 0;
+$more_number = 10;
 ?>
 <div class="page-hair">
 <h2>世界杯帅哥</h2>
@@ -7,8 +8,10 @@
   <span class="icon icon-tag"></span>
   <a href='<?php print url('world_cup/hair'); ?>' ><button class="btn btn-link <?php if (!arg(2)) {print 'btn-active';}?>">全部</button></a>
   <?php foreach ($tags as $tag) :?>
+  <?php $i++; if ($i == $more_number){ print '<span id="more_tags" style="display:none;">';}?>
   <a href='<?php print url('world_cup/hair/'. $tag->tid); ?>' ><button class="btn btn-link <?php if (arg(2) == $tag->tid) {print 'btn-active';}?>"><?php print $tag->name; ?></button></a>
   <?php endforeach;?>
+  <?php if ($i >= $more_number){ print '</span><button id="more_tags_button" class="btn btn-link">更多</button>';}?>
 </div>
 <div class="table-stars">
   <div>
@@ -20,3 +23,21 @@
   </div>
 </div>
 </div>
+
+<script type="text/javascript">
+(function ($) {
+  Drupal.behaviors.Beauty_hair = {
+  attach: function (context) {
+      $('.tags #more_tags_button').click(function(){
+          if ($('#more_tags').css('display') == 'none') {
+              $('#more_tags').show();
+              $('#more_tags_button').text('收起');
+          }
+          else {
+              $('#more_tags').hide();
+              $('#more_tags_button').text('更多');              
+          }
+      });
+  }};
+})(jQuery);
+</script>
