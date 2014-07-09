8426b9f3c393ee089b0577174375acc878dbc707
Boy <andong135@126.com>
2014-07-09 17:29:44 +0800

    整合剪个球星头前端页面 close #10

--- sites/all/themes/beauty/templates/page--beauty-world-cup-hair-advantage.tpl.php
+++ sites/all/themes/beauty/templates/page--beauty-world-cup-hair-advantage.tpl.php
@@ -13,26 +13,6 @@ $base_path = base_path();
     <link href="<?php print $base_path . $directory; ?>/css/ratchet.css" rel="stylesheet">
   </head>
   <body>
-    <div class="content">
-      <div class="banner">
-        <a href="http://v.youku.com/v_show/id_XNzM0ODYzNDQ0.html" title="丽锦发型-C罗">
-          <img src="<?php print $base_path . $directory; ?>/images/cluo.jpg" alt="">
-          <div class="banner-title">【独家】如何打造C罗的发型</div>
-        </a>
-      </div>
-      <div class="page-hair-advantage">
-        <div class="table-stars">
-          <div>
-            <a href="http://v.youku.com/v_show/id_XNzM0ODYzNDQ0.html" title="丽锦发型-C罗">
-              <img src="<?php print $base_path . $directory; ?>/images/cluo.jpg" alt="">
-              <div class="hair-title">【独家】如何打造C罗的发型</div>
-            </a>
-            <a href="http://v.youku.com/v_show/id_XNzM0ODY0Mjg0.html" title="">
-              <img src="<?php print $base_path . $directory; ?>/images/dela.jpg" alt="">
-              <div class="hair-title">【独家】如何打造德拉的发型</div>
-            </a>
-          </div>
-        </div>
-      </div>
+    <?php print($page['content']['system_main']['main']['#markup']);?>
   </body>
 </html>
