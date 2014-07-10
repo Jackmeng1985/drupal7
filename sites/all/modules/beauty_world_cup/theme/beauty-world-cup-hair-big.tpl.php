8b9a8825c33ff3fd6182f3e528f40cc5ba1371fd
jack meng <mengweifu1985@gmail.com>
2014-07-10 10:38:12 +0800

    #6 修改发型详细页面的样式

--- sites/all/modules/beauty_world_cup/theme/beauty-world-cup-hair-big.tpl.php
+++ sites/all/modules/beauty_world_cup/theme/beauty-world-cup-hair-big.tpl.php
@@ -11,17 +11,18 @@
     <!--<div class="slider-num"><span class="active-num">1</span>/<span class="count-num">60</span></div>-->
   </div>
   <ul class="table-view">
-    <li class="table-view-cell media">
+    <li class="table-view-cell media" style="border-bottom:0px;">
       <a class="">
         <span class="media-object pull-left media-title">姓名</span>
         <div class="media-body media-text"><?php print $hairstyle->name;?></div>
       </a>
     </li>
-    <li class="table-view-cell media">
+    <li class="table-view-cell media" style="border-bottom:0px;">
       <a class="">
         <span class="media-object pull-left media-title">打理要点</span>
         <div class="media-body media-text"><?php print $hairstyle->description;?></div>
       </a>
     </li>
   </ul>
-    <div class="btn-wrapper"><a href="<?php print url('world_cup/hair/upload/' .$hairstyle->hid)?>"><button class="btn btn-positive btn-block">换发型</button></a></div>
+    <div class="btn-wrapper"><a href="<?php print url('world_cup/hair/upload/' .$hairstyle->hid)?>"><button class="btn btn-positive btn-block" style="margin-left: auto;
+margin-right: auto;">换发型</button></a></div>
