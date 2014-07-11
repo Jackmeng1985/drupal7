cae6ba2ccceeccdcb57523b8f7539295189137e8
jack meng <mengweifu1985@gmail.com>
2014-07-11 14:22:55 +0800

    #13 翻牌小游戏 前端修改

--- sites/all/modules/beauty_world_cup/js/flip_card.js
+++ sites/all/modules/beauty_world_cup/js/flip_card.js
@@ -1,60 +1,77 @@
 (function($) {
 
     Drupal.beauty_flip_card = {};
+    Drupal.beauty_flip_card.card = 1;
+    Drupal.beauty_flip_card.cardItems = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+    Drupal.beauty_flip_card.triggerIndex = false;
+    Drupal.beauty_flip_card.triggered = false;
+    
     $(document).ready(function() {
         //Initiliaze
-        Drupal.beauty_flip_card.itemShow = 2;
+        Drupal.beauty_flip_card.itemShow = 13;
         Drupal.beauty_flip_card.indexedItemShow = Drupal.beauty_flip_card.itemShow - 1
         Drupal.beauty_flip_card.itemList = $('#portfolio-item');
         Drupal.beauty_flip_card.itemWrapper = $('#portfolio');
-        Drupal.beauty_flip_card.rotation = ['flipped-vertical-bottom', 'flipped-vertical-top', 'flipped-horizontal-left', 'flipped-horizontal-right'];
-        Drupal.beauty_flip_card.navigation = $('#navigation a');
+        Drupal.beauty_flip_card.rotation = 'flipped-horizontal-right'; // 'flipped-vertical-bottom', 'flipped-vertical-top', 'flipped-horizontal-left', 
 
         //Populate items
         for (var i = 0; i < Drupal.beauty_flip_card.itemShow; i++) {
 
             var itemImage = Drupal.beauty_flip_card.itemList.children('li:eq(' + i + ')').children('img');
             var itemSrc = itemImage.attr('src');
-            var itemAlt = itemImage.attr('alt');
-            var item = '<div style="background:url(' + itemSrc + ')" onClick="javascript:Drupal.beauty_flip_card.change_card();"> <span>' + itemAlt + '</span></div>';
+            var item = '<div style="background:url(' + itemSrc + ')" onClick="javascript:Drupal.beauty_flip_card.click_card(this);"></div>';
             Drupal.beauty_flip_card.itemWrapper.append(item);
         }
     });
             
 
 
-    Drupal.beauty_flip_card.change_card = function(context) {
+    Drupal.beauty_flip_card.click_card = function(card) {
+        var $this = Drupal.beauty_flip_card;
+        if ($this.triggered) return;
+        var indexReal = $this.itemWrapper.find('div').index($(card));
+        $this.triggerIndex = indexReal;
+        var item =$this.itemWrapper.children('div:eq(' + indexReal + ')');
+        var animation = $this.rotation;
+        item.addClass('animated ' + animation);
+        $this.triggered = true;
 
-            var page = 2;
-
-            for (var i = 0; i <= Drupal.beauty_flip_card.indexedItemShow; i++) {
-
-                var random = Math.floor(Math.random() * (3 - 0 + 1));
-                var animation = Drupal.beauty_flip_card.rotation[random];
-
-                var item = Drupal.beauty_flip_card.itemWrapper.children('div:eq(' + i + ')');
-
-                item.addClass('animated ' + animation);
-
-                window.setTimeout(function(index) {
-                    return function() {
-
-                        var indexReal = (page == 1) ? index : (index + (page - 1));
-                        var itemHost = indexReal + (Drupal.beauty_flip_card.indexedItemShow * (page - 1));
-
-                        var itemImage = Drupal.beauty_flip_card.itemList.children('li:eq(' + itemHost + ')').children('img');
-                        var itemSrc = itemImage.attr('src');
-                        var itemAlt = itemImage.attr('alt');
-                        var itemCurrent = Drupal.beauty_flip_card.itemWrapper.children('div:eq(' + index + ')');
-
-                        itemCurrent.css('background', 'url(' + itemSrc + ')');
-                        itemCurrent.children('span').text(itemAlt);
-                    };
-                }(i), 500);
-
-                item.bind('transitionend webkitTransitionEnd MSTransitionEnd oTransitionEnd', function() {
-                    $(this).removeClass();
-                });
-            }
-        };
+        item.bind('transitionend webkitTransitionEnd MSTransitionEnd oTransitionEnd', function() {
+            var itemHost = $this.card + $this.indexedItemShow;
+            var itemImage = $this.itemList.children('li:eq(' + itemHost + ')').children('img');
+            var itemSrc = itemImage.attr('src');
+            var itemCurrent = $this.itemWrapper.children('div:eq(' + indexReal + ')');
+            itemCurrent.css('background', 'url(' + itemSrc + ')');
+            $(this).removeClass();
+            $this.flip_card(0);
+        });
+    };
+    
+    Drupal.beauty_flip_card.flip_card = function (index) {
+        var $this = Drupal.beauty_flip_card;
+        
+        if ($this.triggerIndex == index) {
+            $this.flip_card(index + 1);
+            return;
+        } 
+        
+        var item = $this.itemWrapper.children('div:eq(' + index + ')');
+        var animation = $this.rotation;
+        item.addClass('animated ' + animation);   
+        item.bind('transitionend webkitTransitionEnd MSTransitionEnd oTransitionEnd', function() {
+            var itemHost = $this.get_radom_card() + $this.indexedItemShow;
+            var itemSrc = $this.itemList.children('li:eq(' + itemHost + ')').children('img').attr('src');
+            $this.itemWrapper.children('div:eq(' + index + ')').css('background', 'url(' + itemSrc + ')');
+            $(this).removeClass();
+            if ($this.cardItems.length > 0) {
+                $this.flip_card(index + 1);
+            };            
+        });
+    }
+    
+    Drupal.beauty_flip_card.get_radom_card = function () {
+         var random = Math.floor(Math.random() * (12 + 1));
+         return random;
+    }
+    
 })(jQuery);
--- sites/all/modules/beauty_world_cup/theme/beauty-world-cup-flip-card.tpl.php
+++ sites/all/modules/beauty_world_cup/theme/beauty-world-cup-flip-card.tpl.php
@@ -7,9 +7,34 @@ $base_path = base_path();
     <div id="portfolio"></div>
     
     <ul id="portfolio-item">
-      <li><img src="<?php print $base_path . $directory; ?>/images/1.jpeg" alt="Dump Truck" /></li>
-      <li><img src="<?php print $base_path . $directory; ?>/images/2.jpeg" alt="Rrwooo" /></li>
-      <li><img src="<?php print $base_path . $directory; ?>/images/3.jpeg" alt="Dozer" /></li>
-      <li><img src="<?php print $base_path . $directory; ?>/images/4.jpeg" alt="Ltah" /></li>
+      <li><img src="<?php print $base_path . $directory; ?>/images/fan.jpg" alt="" /></li>
+      <li><img src="<?php print $base_path . $directory; ?>/images/fan.jpg" alt="" /></li>
+      <li><img src="<?php print $base_path . $directory; ?>/images/fan.jpg" alt="" /></li>
+      <li><img src="<?php print $base_path . $directory; ?>/images/fan.jpg" alt="" /></li>
+      <li><img src="<?php print $base_path . $directory; ?>/images/fan.jpg" alt="" /></li>
+      <li><img src="<?php print $base_path . $directory; ?>/images/fan.jpg" alt="" /></li>
+      <li><img src="<?php print $base_path . $directory; ?>/images/fan.jpg" alt="" /></li>
+      <li><img src="<?php print $base_path . $directory; ?>/images/fan.jpg" alt="" /></li>
+      <li><img src="<?php print $base_path . $directory; ?>/images/fan.jpg" alt="" /></li>
+      <li><img src="<?php print $base_path . $directory; ?>/images/fan.jpg" alt="" /></li>
+      <li><img src="<?php print $base_path . $directory; ?>/images/fan.jpg" alt="" /></li>
+      <li><img src="<?php print $base_path . $directory; ?>/images/fan.jpg" alt="" /></li>
+      <li><img src="<?php print $base_path . $directory; ?>/images/fan.jpg" alt="" /></li>
+      
+      <li><img src="<?php print $base_path . $directory; ?>/images/1.jpg" alt="" /></li>
+      <li><img src="<?php print $base_path . $directory; ?>/images/2.jpg" alt="" /></li>
+      <li><img src="<?php print $base_path . $directory; ?>/images/3.jpg" alt="" /></li>
+      <li><img src="<?php print $base_path . $directory; ?>/images/4.jpg" alt="" /></li>
+      <li><img src="<?php print $base_path . $directory; ?>/images/5.jpg" alt="" /></li>
+      <li><img src="<?php print $base_path . $directory; ?>/images/6.jpg" alt="" /></li>
+      <li><img src="<?php print $base_path . $directory; ?>/images/7.jpg" alt="" /></li>
+      <li><img src="<?php print $base_path . $directory; ?>/images/8.jpg" alt="" /></li>
+      <li><img src="<?php print $base_path . $directory; ?>/images/9.jpg" alt="" /></li>
+      <li><img src="<?php print $base_path . $directory; ?>/images/10.jpg" alt="" /></li>
+      <li><img src="<?php print $base_path . $directory; ?>/images/J.jpg" alt="" /></li>
+      <li><img src="<?php print $base_path . $directory; ?>/images/Q.jpg" alt="" /></li>
+      <li><img src="<?php print $base_path . $directory; ?>/images/K.jpg" alt="" /></li>
+      
+      
     </ul>
   </div>  
