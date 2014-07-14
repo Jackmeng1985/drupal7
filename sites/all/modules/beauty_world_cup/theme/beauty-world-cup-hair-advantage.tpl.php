e5108d64fde4d45bdd21df0e94b8cd52079828e5
jack meng <mengweifu1985@gmail.com>
2014-07-14 17:01:05 +0800

    #6 增加video页面

--- sites/all/modules/beauty_world_cup/theme/beauty-world-cup-hair-advantage.tpl.php
+++ sites/all/modules/beauty_world_cup/theme/beauty-world-cup-hair-advantage.tpl.php
@@ -1,6 +1,6 @@
 <div class="content">
   <div class="banner">
-    <a href="<?php $url = field_get_items('node', $banner_video, 'field_video_url'); print $url[0]['url']; ?>" title="丽锦发型-C罗">
+    <a href="<?php print url('world_cup/video/'. $banner_video->nid);?>" title="<?php print $banner_video->title;?>">
       <img src="<?php $image = field_get_items('node', $banner_video, 'field_video_thumb'); print image_style_url('video_thumb', $image[0]['uri']); ?>" alt="<?php print $banner_video->title;?>">
       <div class="banner-title"><?php print $banner_video->title;?></div>
     </a>
@@ -9,7 +9,7 @@
   <div class="page-hair-advantage">
     <div class="table-stars">
       <?php foreach ($advantage_videos as $video): ?>
-        <a href="<?php $url = field_get_items('node', $video, 'field_video_url'); print $url[0]['url']; ?>" title="<?php print $video->title;?>">
+        <a href="<?php print url('world_cup/video/'. $video->nid);?>" title="<?php print $video->title;?>">
           <img src="<?php $image = field_get_items('node', $video, 'field_video_thumb'); print image_style_url('video_thumb', $image[0]['uri']); ?>" alt="<?php print $video->title;?>">
           <div class="hair-title"><?php print $video->title;?></div>
         </a>
---
+++ sites/all/modules/beauty_world_cup/theme/beauty-world-cup-video.tpl.php
@@ -0,0 +1,3 @@
+<script type="text/javascript">
+  document.write(' <iframe height='+ document.getElementsByTagName("body")[0].clientHeight+ ' width='+document.getElementsByTagName("body")[0].clientWidth+' src="<?php $url = field_get_items('node', $video, 'field_video_url'); print $url[0]['url']; ?>" frameborder=0 allowfullscreen></iframe>')
+</script>
