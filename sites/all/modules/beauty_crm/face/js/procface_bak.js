91a01f0a007d293b2033704940144090e457aa45
jack meng <mengweifu1985@gmail.com>
2014-07-24 18:07:52 +0800

    #8 优化换发型代码。

--- sites/all/modules/beauty_crm/face/js/procface_bak.js
+++ sites/all/modules/beauty_crm/face/js/procface_bak.js
@@ -1,7 +1,66 @@
 var isTouchSupported = 'ontouchstart' in window;
 var startEvent = isTouchSupported ? 'touchstart' : 'mousedown';
 var moveEvent = isTouchSupported ? 'touchmove' : 'mousemove';
-var temppoint = [];
+var endEvent = isTouchSupported ? 'touchend' : 'mouseup';
+window.opmode = 0;
+window.hairposition = [];
+window.hairscale = 1;
+window.faceposition = [];
+window.temppoint = [];
+window.temppoint2 = [];
+window.setpoints = true;
+
+
+
+function changeHairPointsByXY(x, y){
+    for(var i = 0; i< window.p.length; i++){
+            window.p[i].x = window.p[i].x + x;
+            window.p[i].y = window.p[i].y + y;
+    }
+ }
+
+
+function changeHairPointsByScale(scale){
+    for(var i = 0; i< window.p.length; i++){
+        window.p[i].x = hair_points[i].x  * scale ;
+        window.p[i].y = hair_points[i].y  * scale ;
+   }
+}
+
+
+function drawPointsOnHair(ctx){
+   var radius = 7;
+  for(var i = 0; i<window.p.length; i++){
+    ctx.beginPath();
+    ctx.arc(window.p[i].x + window.hairposition['x'], window.p[i].y + window.hairposition['y'], radius, 0, 2 * Math.PI, false);
+    ctx.fillStyle = 'green';
+    ctx.fill();
+    ctx.stroke();
+  }
+
+
+}
+
+
+
+function drawfacePoint (x,y, color){
+  var c=document.getElementById("face");
+  var ctx=c.getContext("2d");
+  var radius = 7;
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+  ctx.fillStyle = color;
+  ctx.fill();
+  ctx.stroke();
+}
+
+
+function drawHairEyes(){
+  drawfacePoint(hair_eye_left.x, hair_eye_left.y, 'red');
+  drawfacePoint(hair_eye_right.x, hair_eye_right.y, 'red');
+
+}
+
 
 function changeFaceScale(){ //change Face Canvas to 100% of screen
   c = document.getElementById('face');
@@ -9,8 +68,23 @@ function changeFaceScale(){ //change Face Canvas to 100% of screen
   c.height = window.cheight;
 }
 
+function changeHairScale(){ //change front hair Canvas to 100% of hair;
+  c = document.getElementById('hairStyle');
+  c.width = hair_width;
+  c.height = hair_height;
+}
+
+function changeHairChangeScale(){ // we scale the screen to 100% of hair;
+  c = document.getElementById('hairChange');
+  c.width =  hair_width;
+  c.height = hair_height;
+}
+
+
 function   initializeCanvas(){
         changeFaceScale();
+        changeHairScale();
+        changeHairChangeScale();
 }
 
 
@@ -53,18 +127,18 @@ function loadImages(sources, callback) {
 
     
 function calculateHairPosition(){
-  var t_hair_position = [];
+  t_hair_position = [];
   t_hair_position['x'] = 0;
   t_hair_position['y'] = 0;
   //we first get the middle points between eyes of face.
 
-  var face_eye_center = [];
+  face_eye_center = [];
 //  console.log(face_position);
   face_eye_center['x'] = (face_position.eye_right.x + face_position.eye_left.x)/2 ;
   face_eye_center['y'] = (face_position.eye_right.y + face_position.eye_left.y)/2 ;
  ////we then get the middle points between eyes of hairstyle.
 
-  var hair_eye_center = [];
+  hair_eye_center = [];
   hair_eye_center['x']  = (hair_eye_right.x + hair_eye_left.x) /2;
   hair_eye_center['y']  = (hair_eye_right.y + hair_eye_left.y) /2;
 
@@ -80,8 +154,8 @@ function calculateHairPosition(){
 
 function calculateHairScale(){
 
-  var face_scale = face_position.eye_right.x - face_position.eye_left.x;
-  var hair_scale = hair_eye_right.x-hair_eye_left.x;
+  face_scale = face_position.eye_right.x - face_position.eye_left.x;
+  hair_scale = hair_eye_right.x-hair_eye_left.x;
 // we first need to scale hair_scale according to screen;
 //  initial_scale = hair_width /(window.cwidth);
   window.hairscale =  face_scale / hair_scale;
@@ -93,6 +167,7 @@ function calculateHairScale(){
 function changeScale(scale, change){
 
    window.hairscale = scale + change;
+   changeHairPointsByScale(scale);
 
    var canvas = document.getElementById('face');
    var context = canvas.getContext('2d');
@@ -125,7 +200,7 @@ function changePosition(x, y){
 function getMergedImage(face_url, hair_url, face_position, hair_position, hair_scale){//put hair sytle on top of face
   // first we need to get face image data
 
-  var canvas = document.getElementById('face');
+  canvas = document.getElementById('face');
   var context = canvas.getContext('2d');
   var sources = { face: face_url, hair: hair_url};
 
@@ -144,6 +219,55 @@ function getMergedImage(face_url, hair_url, face_position, hair_position, hair_s
    });
  }
 
+function changePosition2(x, y){
+   window.hairposition['x'] = window.hairposition['x'] + x;
+   window.hairposition['y'] = window.hairposition['y'] + y;
+
+ //  changeHairPointsByXY(x, y);
+
+   var canvas = document.getElementById('face');
+   var context = canvas.getContext('2d');
+   //context.clearRect(0, 0, canvas.width, canvas.height);
+   getMergedImage2(face_url, hair_url, window.faceposition,window.hairposition, window.hairscale );
+
+
+
+}
+
+
+
+function getMergedImage2(face_url, hair_url, face_position, hair_position, hair_scale){//put hair sytle on top of face
+  // first we need to get face image data
+
+  canvas = document.getElementById('face');
+  var context = canvas.getContext('2d');
+  var sources = { face: face_url, hair: hair_url};
+
+  loadImages(sources, function(images) {
+       context.drawImage(images.face, face_position['x'],face_position['y'],face_width,face_height);;
+
+       faceimgTargetData = context.getImageData(face_position['x'],face_position['y'],face_width, face_height);
+       context.drawImage(images.hair, hair_position['x'],hair_position['y'],images.hair.width * hair_scale,images.hair.height * hair_scale);
+       hairimgTargetData = context.getImageData(hair_position['x'],hair_position['y'],images.hair.width * hair_scale,images.hair.height * hair_scale);
+       //drawPointsOnHair(context);  //draw change points according to scale;
+   });
+ }
+
+function getCurrentPointIndex(q){
+  currentPoint = -1;
+  x = q.x -  window.hairposition['x'];
+  y = q.y -  window.hairposition['y'];
+  k = new Point(x,y);
+  for(var i = 0 ; i< window.p.length; i++){
+        if (  window.p[i].InfintyNormDistanceTo(k) <= 30 ) {
+             currentPoint = i;
+             return i;
+        }
+  }
+  return currentPoint;
+}
+
+
 
 
 
@@ -157,7 +281,20 @@ function touchStart(event){
    window.temppoint['x'] = startX; // this is used to move whole hairstyle
    window.temppoint['y'] = startY;
 
-   return;
+   window.temppoint2['x'] = startX; // this is used to move whole hairstyle
+   window.temppoint2['y'] = startY;
+
+   q = new Point(startX, startY); //create an object for currrent point
+   window.currentPointIndex = getCurrentPointIndex(q); // we get the point index
+   if( window.currentPointIndex >= 0 ) { // here we do all work relative to point move
+       if(window.qbak.length == 0){
+         window.qbak = new Array();
+         for (var i = 0, len = window.p.length; i < len; i++) {
+         window.qbak[i] = new Point(window.p[i].x,window.p[i].y);
+       }
+     }
+       return;
+    } // this part is for change style
 }
 
 
@@ -171,13 +308,61 @@ function touchMove(event){
     startX = touch.pageX;
     startY = touch.pageY;
 
+    if( window.currentPointIndex < 0 ){
       changeX = startX - window.temppoint['x'];
       changeY = startY - window.temppoint['y'];
       changePosition(changeX, changeY);
       window.temppoint['x'] = startX;
       window.temppoint['y'] = startY;
+    } else {
+       return;
+    }
     return;
 }
+
+function touchEnd(event){
+    event.preventDefault();
+    //alert(event.changedTouches[0].pageX);
+    var touch =  isTouchSupported ? event.changedTouches[0] : event;
+
+    startX = touch.pageX;
+    startY = touch.pageY;
+
+    var Scale = 0.5;
+    changeX = (startX - window.temppoint2['x']) * Scale;
+    changeY = (startY - window.temppoint2['y']) * Scale;
+
+    if(changeX == 0 && changeY == 0) {
+      if(window.setpoints) {
+        changePosition2(changeX, changeY);
+        window.setpoints = false;
+      }
+      else {
+        changePosition(changeX, changeY);
+        window.setpoints = true;
+      }
+    }
+
+    if( window.currentPointIndex > 0 ){
+       movedPoint = new Point( parseInt((startX -  window.hairposition['x'])),parseInt((startY -  window.hairposition['y'])));
+       work(movedPoint);  // we move the point
+       window.currentPointIndex = -1;
+    }
+    return;
+}
+
+function initializeFaceData(face_url){
+  canvas = document.getElementById('face');
+  var context = canvas.getContext('2d');
+  source = new Image();
+  source.src = face_url;
+  source.onload = function (){
+    context.drawImage(source,0,0);
+//    window.faceImg = context.getImageData( 0, 0, window.cwidth , window.cheight);
+    //context.clearRect(0, 0, canvas.width, canvas.height);
+  }
+}
+
 
 function savePicture (){
   var canvas = document.getElementById('face');
@@ -201,10 +386,12 @@ function savePicture (){
 //       console.log( window.cwidth) ;
         initializeCanvas();  //change canvas to proper width and height;
 
-        var t_face_position = [];
-        var t_hair_position = [];
+        t_face_position = [];
+        t_hair_position = [];
         t_face_position['x'] = 0;
         t_face_position['y'] = 0;
+        //console.log(document.getElementsByTagName("body")[0].clientWidth);
+       //console.log(document.getElementsByTagName("body")[0].clientHeight);
 
         t_hair_position['x'] = 0;
         t_hair_position['y'] = 0;
@@ -213,18 +400,23 @@ function savePicture (){
         //we need to calculate the position of hair style and scale;
         calculateHairScale();
         window.hairposition = calculateHairPosition();
+        initializePoints(window.hairscale);  //put hair points into window.p, be noticed the point position is relative to upper-left of hairstyle
+       // getMergedImage(face_url, hair_url, window.faceposition,window.hairposition, window.hairscale/10); //put hair sytle on top of face
 
+//        window.hairposition['x'] = 0;
+//        window.hairposition['y'] =0;
+//        window.hairscale  = 1;
 
         $('.bar .scaleup'). click(function (){changeScale(window.hairscale,  0.05);});
         $('.bar .scaledown'). click(function (){changeScale(window.hairscale, -0.05);});
         $('.bar .share'). click(function (){savePicture();});
+         initializeFaceData(face_url);
 
         //drawcanvas();
-        
         getMergedImage(face_url, hair_url, window.faceposition,window.hairposition, window.hairscale); //put hair sytle on top of face
 
         $("#loading").hide();
-        var c = document.getElementById("face");
+        c=document.getElementById("face");
         c.addEventListener(startEvent, touchStart, false);
         c.addEventListener(moveEvent, touchMove, false);
 //        c.addEventListener(endEvent, touchEnd, false);
