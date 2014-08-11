cc7c5d91e0583a47b61ea6fd75c0a8e9ed26fb0b
Boy <andong135@126.com>
2014-08-11 15:26:13 +0800

    #20 增加views模板

---
+++ sites/all/modules/beauty_weibo/themes/views-view-field--weibo-post--status.tpl.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @file
+ * This template is used to print a single field in a view.
+ *
+ * It is not actually used in default Views, as this is registered as a theme
+ * function which has better performance. For single overrides, the template is
+ * perfectly okay.
+ *
+ * Variables available:
+ * - $view: The view object
+ * - $field: The field handler object that can process the input
+ * - $row: The raw SQL result that can be used
+ * - $output: The processed output that will normally be used.
+ *
+ * When fetching output from the $row, this construct should be used:
+ * $data = $row->{$field->field_alias}
+ *
+ * The above will guarantee that you'll always get the correct data,
+ * regardless of any changes in the aliasing that might happen if
+ * the view is modified.
+ */
+?>
+<?php
+  $mid = $row->beauty_weibo_post_wid;
+  if ($row->beauty_weibo_post_status == '0') {
+    $output = l('抓取', '/beauty/ajax/weibo_get_reposts/' . $mid);
+  }
+  else {
+    $output = l('查看', '/weibo/reposts/' . $mid);
+  }
+  print $output;
+?>
