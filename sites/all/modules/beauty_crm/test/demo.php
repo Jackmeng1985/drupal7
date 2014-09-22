fe14c2f17f90d60a073a69f1292add0282c68ca0
jack meng <mengweifu1985@gmail.com>
2014-09-22 17:05:42 +0800

    #4 后台管理 汇总页面

--- sites/all/modules/beauty_crm/test/demo.php
+++ sites/all/modules/beauty_crm/test/demo.php
@@ -1,4 +1,103 @@
 <?php $s = $_SERVER['HTTP_USER_AGENT'];?>
+<?php
+
+$str62keys = array(
+
+"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+
+ "a", "b", "c", "d", "e", "f", "g", "h", "i", 
+
+ "j", "k", "l", "m", "n", "o", "p", "q", 
+
+ "r", "s", "t", "u", "v", "w", "x", "y", 
+
+ "z","A", "B", "C", "D", "E", "F", "G", "H", 
+
+ "I", "J", "K", "L", "M", "N", "O", "P", 
+
+ "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+
+);
+
+
+
+/**
+
+ * 10进制值转换为62进制
+
+ * @param {String} int10 10进制值
+
+ * @return {String} 62进制值
+
+ */
+
+function int10to62($int10) {
+
+ global $str62keys;
+
+ $s62 = '';
+
+ $r = 0;
+
+ while ($int10 != 0)
+
+ {
+
+  $r = $int10 % 62;
+
+  $s62 = $str62keys[$r].$s62;
+
+  $int10 = floor($int10 / 62);
+
+ }
+
+ return $s62;
+
+}
+
+/**
+
+ * 
+
+ * 通过mid获得短格式
+
+ * @param string $mid
+
+ * @return 短格式
+
+ */
+
+function getCodeByMid($mid){
+
+ $url = '';
+
+ for ($i = strlen($mid) - 7; $i > -7; $i -=7) //从最后往前以7字节为一组读取mid
+
+ {
+
+  $offset1 = $i < 0 ? 0 : $i;
+
+  $offset2 = $i + 7;
+
+  $num = substr($mid, $offset1,$offset2-$offset1); 
+
+  //mid.substring(offset1, offset2); 
+
+  $num = int10to62($num);
+
+  $url = $num .$url;
+
+ }
+
+ 
+
+ return $url;
+
+}
+
+echo getCodeByMid('221110410216147026');
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
